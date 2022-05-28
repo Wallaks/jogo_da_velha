32058f66297cb5e01fe48ec8f08aa56e823dd10a
Wallaks <wallaks.login@gmail.com>
2022-05-28 19:22:19 -0300

    mais um teste

--- src/main/java/main/maven/ViewSoma.java
+++
@@ -1,5 +0,0 @@
-package main.maven;
-
-public class ViewSoma {
-
-}
--- src/main/java/main/maven/application.java
+++ src/main/java/main/maven/application.java
@@ -2,17 +2,14 @@ package main.maven;
 
 public class application {
 
-	public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException {
+        int good = 0;
+        boolean bonitao = true;
 
-		
-		
-		int good = 0;
-		boolean bonitao = true;
-
-		while (bonitao) {
-			System.out.println("Wallaks bonitao" + good);
-			good = good + 1;
-			//banana
-		}
-	}
+        while (bonitao) {
+            System.out.println("Wallaks bonitao" + good);
+            good = good + 1;
+            // banana de pijama
+        }
+    }
 }
